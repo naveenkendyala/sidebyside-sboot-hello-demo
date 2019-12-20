fcdccb04146e235a78d649d6db29990100896b50
Naveen Kendyala <naveenkendyala@kendyala.com>
2019-12-19 23:03:28 -0500

    Added scripts and updated the deployment yaml

---
+++ deployments/deploy.sh
@@ -0,0 +1,4 @@
+oc apply -f deployment.yaml
+oc expose deployment.apps/springboot-hello-deploy
+oc expose service/springboot-hello-deploy
+oc autoscale deploy/springboot-hello-deploy --min 1 --max 5 --cpu-percent=40
