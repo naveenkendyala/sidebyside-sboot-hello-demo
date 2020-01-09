7e6ab99e417928996bd09ba77acc0fa231d7c82c
Naveen Kendyala <naveenkendyala@kendyala.com>
2020-01-09 13:52:40 -0500

    Updated max limit on pods

--- deployments/deploy.sh
+++ deployments/deploy.sh
@@ -1,4 +1,4 @@
 oc apply -f deployment.yaml
 oc expose deployment.apps/springboot-hello-deploy
 oc expose service/springboot-hello-deploy
-oc autoscale deploy/springboot-hello-deploy --min 1 --max 5 --cpu-percent=50
+oc autoscale deploy/springboot-hello-deploy --min 1 --max 10 --cpu-percent=50
